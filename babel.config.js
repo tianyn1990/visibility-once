118c64072f9b8c6d50eb8955c9e999a78c3f736a
tianyn1990 <tianyanan.zero@gmail.com>
2019-09-18 00:41:00 +0800

    misc

--- babel.config.js
+++ babel.config.js
@@ -38,6 +38,15 @@ module.exports = (api) => {
                     */
                     useBuiltIns: 'usage',
                     debug: true,
+                    // 主要针对意疑似副作用的函数，使用loose的写法，可以被识别为无副作用的
+                    // 但因为 babel @babel/preset-env 中，会识别无副作用的函数，并添加__PURE__注释（!isProperty && !isIIFE && path.isExpression()）
+                    // 在提供给webpack打包（uglifyjs）时会认为是pure的，按理说不需要loose了
+                    // 但如果rollup打包使用了uglify/uglify-es6，会将这个注释去掉，提供给工程的webpack打包的时候，就没用了，因此要loose
+                    // 补充：loose主要是为了将函数由有副作用变为无副作用，便于webpack打包的时候treeshaking掉
+                    // 补充：webpack里用的uglify没有语法分析功能（rollup有，但不适合工程构建），因此在无法将疑似有副作用的函数treeshaking掉
+                    // 因此：包里面不使用uglify了，这样会添加__PURE__注释，在工程的webpack的uglify执行时，会识别这些注释
+                    // 补充：另一种解决办法，类似elementui，将组件分别output，工程使用的时候引入一个webpack插件，方便使用
+                    // loose: true,
                     // modules: 'commonjs', // https://github.com/webpack/webpack/issues/4039#issuecomment-419284940
                 },
             ],
--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,16 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-import { uglify } from 'rollup-plugin-uglify';
 
-export default {
+// 因为 babel @babel/preset-env 中，会识别无副作用的函数，并添加__PURE__注释（!isProperty && !isIIFE && path.isExpression()）
+// 在提供给webpack打包（uglifyjs）时会认为是pure的
+// 但如果rollup打包使用了uglify/uglify-es6，会将这个注释去掉，提供给工程的webpack打包的时候，就没用了
+// 补充：webpack里用的uglify没有语法分析功能（rollup有，但不适合工程构建），因此在无法将疑似有副作用的函数treeshaking掉
+// 因此：包里面不使用uglify了，这样会添加__PURE__注释，在工程的webpack的uglify执行时，会识别这些注释
+// import { uglify } from 'rollup-plugin-uglify';
+// import uglifyES6 from 'rollup-plugin-uglify-es';
+
+export default [{
     input: 'lib/visibility.js',
     output: {
         file: 'dist/index.js',
@@ -11,7 +18,7 @@ export default {
     },
     // external: [],
     plugins: [
-        uglify(),
+        // uglify(),
         resolve({
             only: ['lib'],
         }),
@@ -21,4 +28,36 @@ export default {
             runtimeHelpers: true,
         }),
     ],
-};
+}, {
+    input: 'lib/visibility.js',
+    output: {
+        file: 'dist/index.mjs',
+        format: 'es',
+    },
+    plugins: [
+        // uglifyES6(),
+        resolve({
+            only: ['lib'],
+        }),
+        commonjs(),
+        babel({
+            exclude: 'node_modules/**', // 只编译我们的源代码
+            runtimeHelpers: true,
+        }),
+    ],
+}, {
+    input: 'lib/visibility.js',
+    output: {
+        file: 'dist/babel6/index.js',
+        format: 'cjs',
+    },
+    plugins: [
+        // uglify(),
+        resolve(),
+        commonjs(),
+        babel({
+            exclude: 'node_modules/**', // 只编译我们的源代码
+            runtimeHelpers: true,
+        }),
+    ],
+}];
